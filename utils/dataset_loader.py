febefdb8672f428c88b77d16f7f95ed1d23682af
r7rohan <rohanmallick711@gmail.com>
2021-03-18 02:59:27 +0530

    training and analysis done

--- utils/dataset_loader.py
+++ utils/dataset_loader.py
@@ -12,7 +12,7 @@ def find_classes(dir):
 class ImageFolder():
     def __init__(self, root, transform=None, target_transform=None, loader = default_loader):
         classes, class_to_idx = find_classes(root)
-        imgs = make_dataset(root, class_to_idx, ("jpg"))
+        imgs = make_dataset(root, class_to_idx, ("jpg","ppm"))
         if len(imgs) == 0:
             raise(RuntimeError("Not found"))
         self.root = root
@@ -37,18 +37,23 @@ class ImageFolder():
 
 
 # List of transforms
-def create_loader(path,batch_size=64,shuffle=True,cudav=False,transform=None,sz=32):
+def create_loader(path,batch_size=64,shuffle=True,cudav=False,transform=None,sz=32,split = 0):
     if transform is None:
         transform = [transforms.Compose([
-        transforms.Resize(sz,sz),
+        transforms.Resize((sz,sz)),
         transforms.ToTensor()
                                         ])]
     ds = []
     for trans in transform:
       ds.append(ImageFolder(path, transform=trans))
     ds = torch.utils.data.ConcatDataset(ds)
-
-    loader = torch.utils.data.DataLoader(ds,batch_size=batch_size, shuffle=shuffle, num_workers=4, pin_memory=cudav)
+    if split!=0:
+        num = int(split*len(ds))
+        train_ds, val_ds = torch.utils.data.random_split(ds, [len(ds)-num,num])
+        train_loader = torch.utils.data.DataLoader(ds,batch_size=batch_size, shuffle=shuffle)
+        val_loader = torch.utils.data.DataLoader(ds,batch_size=batch_size, shuffle=shuffle)
+        return train_loader, val_loader
+    loader = torch.utils.data.DataLoader(ds,batch_size=batch_size, shuffle=shuffle)
     return loader
 
 
